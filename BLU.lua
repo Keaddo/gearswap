1fdce3628f9ca39c4fb5cea75a2d38e740f869d4
Keaddo <mbs@goteki.net>
2016-01-21 12:32:29 +0100

    1.13.1
    Typo

--- BLU.lua
+++ BLU.lua
@@ -38,7 +38,7 @@ function get_sets()
        
         sets.Idle.Standard = {ammo="Jukukik Feather",
                                                   head="Rawhide Mask",neck="Sanctity Necklace", ear1="Suppanomimi", ear2="Brutal Earring",
-                                                  body="Assimilator's Jubbah +1",hands="Adhemar wristebands",ring1="Epona's Ring",ring2="Rajas ring",
+                                                  body="Assimilator's Jubbah +1",hands="Adhemar wristbands",ring1="Epona's Ring",ring2="Rajas ring",
                                                   back="Cornflower Cape",waist="Windbuffet Belt +1",legs="Crimson Cuisses",feet="Herculean Boots"}
                                                  
         sets.Idle.DT = {ammo="Vanir Battery",
