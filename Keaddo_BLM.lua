27f3b015c766fac6e0d9ada6bc06c3f02b5aaf9f
Keaddo <mbs@goteki.net>
2016-08-26 08:56:32 +0200

    BLM 1.5
    fixed death

--- Keaddo_BLM.lua
+++ Keaddo_BLM.lua
@@ -103,9 +103,11 @@ function init_gear_sets()
 		
     sets.defense.PDT =                                                      set_combine(sets.idle,{
 		body = "Merlinic Jubbah",
+		ammo = "Staunch Tathlum",
 		neck="Loricate Torque +1",
 		ear1="Etiolation Earring",
-		ring2="Gelatinous Ring +1",
+		ear2="Genmei Earring",
+		ring2="Warden's Ring",
 		ring1="Defending Ring"
 		})
 
@@ -373,14 +375,35 @@ function init_gear_sets()
 	})
 
     sets.magic_burst.Death = {
-		neck="Mizukage-no-Kubikazari",
-		ring1="Mujin Band",
+    ammo="Psilomene",
+    head="Pixie Hairpin +1",
+    body={ name="Amalric Doublet", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    hands={ name="Amalric Gages", augments={'INT+10','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    legs={ name="Amalric Slops", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    feet={ name="Merlinic Crackows", augments={'"Mag.Atk.Bns."+25','Magic burst mdg.+9%','CHR+6','Mag. Acc.+13',}},
+    neck="Mizu. Kubikazari",
+    waist="Hachirin-no-Obi",
+    left_ear="Barkaro. Earring",
+    right_ear="Static Earring",
+    left_ring="Mujin Band",
+    right_ring="Mephitas's Ring +1",
+    back={ name="Taranus's Cape", augments={'MP+60','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10',}},
     }
 
     sets.magic_burst.Death.HighMP = set_combine(sets.midcast.Death.HighMP,{
-		neck="Mizukage-no-Kubikazari",
-		ear2="Static Earring",
-		ring1="Mujin Band"
+    ammo="Psilomene",
+    head="Pixie Hairpin +1",
+    body={ name="Amalric Doublet", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    hands={ name="Amalric Gages", augments={'INT+10','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    legs={ name="Amalric Slops", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    feet={ name="Merlinic Crackows", augments={'"Mag.Atk.Bns."+25','Magic burst mdg.+9%','CHR+6','Mag. Acc.+13',}},
+    neck="Mizu. Kubikazari",
+    waist="Hachirin-no-Obi",
+    left_ear="Barkaro. Earring",
+    right_ear="Static Earring",
+    left_ring="Mujin Band",
+    right_ring="Mephitas's Ring +1",
+    back={ name="Taranus's Cape", augments={'MP+60','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10',}},
 		
     })
     
---
+++ export/Keaddo 22 10 38PM  16-15-08.lua
@@ -0,0 +1,17 @@
+sets.exported={
+    main={ name="Lathi", augments={'MP+80','INT+20','"Mag.Atk.Bns."+20',}},
+    sub="Niobid Strap",
+    ammo="Psilomene",
+    head="Pixie Hairpin +1",
+    body={ name="Amalric Doublet", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    hands={ name="Amalric Gages", augments={'INT+10','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    legs={ name="Amalric Slops", augments={'MP+60','Mag. Acc.+15','"Mag.Atk.Bns."+15',}},
+    feet={ name="Merlinic Crackows", augments={'"Mag.Atk.Bns."+25','Magic burst mdg.+9%','CHR+6','Mag. Acc.+13',}},
+    neck="Mizu. Kubikazari",
+    waist="Hachirin-no-Obi",
+    left_ear="Barkaro. Earring",
+    right_ear="Static Earring",
+    left_ring="Mujin Band",
+    right_ring="Mephitas's Ring +1",
+    back={ name="Taranus's Cape", augments={'MP+60','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10',}},
+}
